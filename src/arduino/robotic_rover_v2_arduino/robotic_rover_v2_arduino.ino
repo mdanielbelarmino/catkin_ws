5e44697e6df4c92625bd28d952e0b14dee67d970
mark daniel belarmino <daniel_belarmino@ymail.com>
2022-04-22 18:51:18 +0800

    new arduino code

--- src/arduino/robotic_rover_v2_arduino/robotic_rover_v2_arduino.ino
+++ src/arduino/robotic_rover_v2_arduino/robotic_rover_v2_arduino.ino
@@ -1,60 +1,34 @@
-/*Data Format
-   LeftMotor Speed, RightMotor Speed, Headlight Intensity, Picker(bool), AUX1, AUX2
-    255,255,50,1,0,0%
-*/
-
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3Stamped.h>
 #include <ros/time.h>
 
-#define pwm_pinR2 9
-#define dir_pinR2 17
-#define pwm_pinR 11
-#define dir_pinR 23
-#define pwm_pinL2 8
-#define dir_pinL2 16
-#define pwm_pinL 10
-#define dir_pinL 22
+#define pwm_pinL 5
+#define dir_pinL 4
+#define pwm_pinR 6
+#define dir_pinR 7
 
-#define front_tof_pin A0
-#define rear_tof_pin A1
-
-#define MAX_SPEED 22
+#define MAX_SPEED 0.2
 #define LOOPTIME 100
-#define MAX_PWM 85
-#define ROT_SPEED 20
-
-#define prop 0.05
 
 int t2 = 0;
 int t1;
 int period;
 
-volatile int pulseL = 0;
-volatile int pulseR = 0;
-volatile int pulseL2 = 0;
-volatile int pulseR2 = 0;
+volatile unsigned int pulseL = 0;
+volatile unsigned int pulseR = 0;
 double rpmL;
 double rpmR;
-double rpmL2;
-double rpmR2;
 double curr_spdL; // cm/s
 double des_spdL; // cm/s
 double curr_spdR; // cm/s
 double des_spdR; // cm/s
-double curr_spdL2; // cm/s
-double des_spdL2; // cm/s
-double curr_spdR2; // cm/s
-double des_spdR2; // cm/s
-int16_t pwmL = 0;
-int16_t pwmR = 0;
-int16_t pwmL2 = 0;
-int16_t pwmR2 = 0;
+uint16_t pwmL = 0;
+uint16_t pwmR = 0;
 
-// Add direction parameter here
-
-double gen_speed = MAX_SPEED;
+// Kinematics Constant
+const float wheel_base = 0.31;
+const float wheel_radius = 0.055;
 
 void isrL() //interrupt service routine
 {
@@ -66,136 +40,88 @@ void isrR() //interrupt service routine
   pulseR++;
 }
 
-void isrL2() //interrupt service routine
-{
-  pulseL2++;
-}
-
-void isrR2() //interrupt service routine
-{
-  pulseR2++;
-}
-
 ros::NodeHandle  nh;
 String rec;
+// for debugging purposes
 int state = 0;
+
 
 void messageCb( const geometry_msgs::Twist& cmd)
 {
 
-  float spd = cmd.linear.x;
-  float ang = cmd.angular.z;
+  double linear_velocity = cmd.linear.x;
+  double angular_velocity = cmd.angular.z;
 
-  if (spd > 0 && ang == 0) //forward
+  angular_velocity = (angular_velocity < 0.0001 && angular_velocity > -0.0001) ? 0.0 : angular_velocity;
+  linear_velocity = (linear_velocity < 0.0001 && linear_velocity > -0.0001) ? 0.0 : linear_velocity;
+
+  double Vl = (linear_velocity - angular_velocity * wheel_base / 2) / wheel_radius;
+  double Vr = (linear_velocity + angular_velocity * wheel_base / 2) / wheel_radius;
+
+  des_spdL = abs(Vl);
+  des_spdR = abs(Vr);
+
+  if (linear_velocity > 0.0 && angular_velocity == 0.0) //forward
   {
-    des_spdL = gen_speed;
-    des_spdR = gen_speed;
-    des_spdL2 = gen_speed;
-    des_spdR2 = gen_speed;
     digitalWrite(dir_pinL, HIGH); // forward
     digitalWrite(dir_pinR, LOW); // forward
-    digitalWrite(dir_pinL2, HIGH); // forward
-    digitalWrite(dir_pinR2, LOW); // forward
     state = 0;
-  }
-  else if (spd < 0 && ang == 0) //reverse
+  } 
+  else if (linear_velocity < 0.0 && angular_velocity == 0.0) //reverse
   {
-    des_spdL = gen_speed;
-    des_spdR = gen_speed;
-    des_spdL2 = gen_speed;
-    des_spdR2 = gen_speed;
     digitalWrite(dir_pinL, LOW);
     digitalWrite(dir_pinR, HIGH);
-    digitalWrite(dir_pinL2, LOW);
-    digitalWrite(dir_pinR2, HIGH);
     state = 1;
   }
 
-  else if (ang < 0 && spd == 0) // right
+  else if (angular_velocity < 0.0 && linear_velocity == 0.0) // turn right
   {
-    des_spdL = ROT_SPEED;
-    des_spdR = ROT_SPEED;
-    des_spdL2 = ROT_SPEED;
-    des_spdR2 = ROT_SPEED;
     digitalWrite(dir_pinL, HIGH);
     digitalWrite(dir_pinR, HIGH);
-    digitalWrite(dir_pinL2, HIGH);
-    digitalWrite(dir_pinR2, HIGH);
     state = 2;
   }
-  else if ( ang > 0 && spd == 0) // left
+  else if ( angular_velocity > 0.0 && linear_velocity == 0.0) // turn left
   {
-    des_spdL = ROT_SPEED;
-    des_spdR = ROT_SPEED;
-    des_spdL2 = ROT_SPEED;
-    des_spdR2 = ROT_SPEED;
     digitalWrite(dir_pinL, LOW);
     digitalWrite(dir_pinR, LOW);
-    digitalWrite(dir_pinL2, LOW);
-    digitalWrite(dir_pinR2, LOW);
     state = 3;
   }
-  
-  // code if both spd and ang has value
-  
-  else if ( ang < 0 && spd > 0) // right forward
+
+  // code if both linear_velocity and angular_velocity has value
+
+  else if ( angular_velocity < 0.0 && linear_velocity > 0.0) // steer right
   {
-    des_spdL = ROT_SPEED;
-    des_spdR = ROT_SPEED/4;
-    des_spdL2 = ROT_SPEED;
-    des_spdR2 = ROT_SPEED/4;
-    digitalWrite(dir_pinL, HIGH);
-    digitalWrite(dir_pinR, HIGH);
-    digitalWrite(dir_pinL2, HIGH);
-    digitalWrite(dir_pinR2, HIGH);
+    digitalWrite(dir_pinL, HIGH); // forward
+    digitalWrite(dir_pinR, LOW); // forward
     state = 4;
   }
 
-    else if ( ang > 0 && spd > 0) // left forward
+  else if ( angular_velocity > 0.0 && linear_velocity > 0.0) // steer left
   {
-    des_spdL = ROT_SPEED/4;
-    des_spdR = ROT_SPEED;
-    des_spdL2 = ROT_SPEED/4;
-    des_spdR2 = ROT_SPEED;
-    digitalWrite(dir_pinL, LOW);
-    digitalWrite(dir_pinR, LOW);
-    digitalWrite(dir_pinL2, LOW);
-    digitalWrite(dir_pinR2, LOW);
+    digitalWrite(dir_pinL, HIGH); // forward
+    digitalWrite(dir_pinR, LOW); // forward
     state = 5;
   }
 
-  else if ( ang > 0 && spd < 0) // right reverse
+  else if ( angular_velocity > 0.0 && linear_velocity < 0.0) // steer right reverse
   {
-    des_spdL = ROT_SPEED;
-    des_spdR = ROT_SPEED/4;
-    des_spdL2 = ROT_SPEED;
-    des_spdR2 = ROT_SPEED/4;
     digitalWrite(dir_pinL, LOW);
-    digitalWrite(dir_pinR, LOW);
-    digitalWrite(dir_pinL2, LOW);
-    digitalWrite(dir_pinR2, LOW);
-    state = 4;
-  }
-
-    else if ( ang < 0 && spd < 0) // left reverse
-  {
-    des_spdL = ROT_SPEED/4;
-    des_spdR = ROT_SPEED;
-    des_spdL2 = ROT_SPEED/4;
-    des_spdR2 = ROT_SPEED;
-    digitalWrite(dir_pinL, HIGH);
     digitalWrite(dir_pinR, HIGH);
-    digitalWrite(dir_pinL2, HIGH);
-    digitalWrite(dir_pinR2, HIGH);
-    state = 5;
+    state = 6;
   }
-  
-  else if (spd == 0)
+
+  else if ( angular_velocity < 0.0 && linear_velocity < 0.0) // steer left reverse
+  {
+    digitalWrite(dir_pinL, LOW);
+    digitalWrite(dir_pinR, HIGH);
+    state = 7;
+  }
+
+  else if (linear_velocity == 0.0 && angular_velocity == 0.0) // stop
   {
     des_spdL = 0;
     des_spdR = 0;
-    des_spdL2 = 0;
-    des_spdR2 = 0;
+    state = 8;
   }
 }
 
@@ -210,129 +136,72 @@ void setup()
   nh.subscribe(sub);
   nh.advertise(speed_pub);
 
-
-  attachInterrupt(4, isrR2, RISING);
-  attachInterrupt(2, isrR, RISING);
-  attachInterrupt(5, isrL2, RISING);
-  attachInterrupt(3, isrL, RISING);
+  attachInterrupt(0, isrL, RISING); //attaching the interrupt to pin
+  attachInterrupt(1, isrR, RISING); //attaching the interrupt to pin
 
   pinMode(pwm_pinL, OUTPUT);
   pinMode(dir_pinL, OUTPUT);
   pinMode(pwm_pinR, OUTPUT);
   pinMode(dir_pinR, OUTPUT);
-  pinMode(pwm_pinL2, OUTPUT);
-  pinMode(dir_pinL2, OUTPUT);
-  pinMode(pwm_pinR2, OUTPUT);
-  pinMode(dir_pinR2, OUTPUT);
-  pinMode(front_tof_pin, INPUT);
-  pinMode(rear_tof_pin, INPUT);
 
   analogWrite(pwm_pinR, 0);
   analogWrite(pwm_pinL, 0);
-  analogWrite(pwm_pinR2, 0);
-  analogWrite(pwm_pinL2, 0);
-
-  digitalWrite(dir_pinL, HIGH); // forward
-  digitalWrite(dir_pinR, LOW); // forward
-  digitalWrite(dir_pinL2, HIGH); // forward
-  digitalWrite(dir_pinR2, LOW); // forward
-
+  digitalWrite(dir_pinL, HIGH);
+  digitalWrite(dir_pinR, LOW);
 }
-
-int ctr = 0;
 
 void loop()
 {
-
-  detachInterrupt(4);           //detaches the interrupt pin 19 L old
-  detachInterrupt(2);           //detaches the interrupt pin 21 R
-  detachInterrupt(5);           //detaches the interrupt pin 18 L2
-  detachInterrupt(3);           //detaches the interrupt pin 20 R2
+  detachInterrupt(0);           //detaches the interrupt
+  detachInterrupt(1);           //detaches the interrupt
 
   t1 = millis();    //finds the time
-  bool front_tof = digitalRead(front_tof_pin);
-  bool rear_tof = digitalRead(rear_tof_pin);
 
   period = t1 - t2;
   if (period >= 10)
   {
 
-    float lp = pulseL;
-    float rp = pulseR;
-    float lp2 = pulseL2;
-    float rp2 = pulseR2;
+    double lp = pulseL;
+    double rp = pulseR;
+    rpmL = (lp / 43) * 60;
+    rpmR = (rp / 43) * 60;
 
-    rpmL = (lp / 0.43) * 60;
-    rpmR = (rp / 0.43) * 60;
-    rpmL2 = (lp2 / 0.43) * 60;
-    rpmR2 = (rp2 / 0.43) * 60;
-
-    curr_spdL = (rpmL / 60) * (2 * PI * 0.08) * 100;
-    curr_spdR = (rpmR / 60) * (2 * PI * 0.08) * 100;
-    curr_spdL2 = (rpmL2 / 60) * (2 * PI * 0.08) * 100;
-    curr_spdR2 = (rpmR2 / 60) * (2 * PI * 0.08) * 100;
+    curr_spdL = (rpmL / 60) * (2 * PI * 0.055) * 100;
+    curr_spdR = (rpmR / 60) * (2 * PI * 0.055) * 100;
 
     pulseL = 0;
     pulseR = 0;
-    pulseL2 = 0;
-    pulseR2 = 0;
 
     t2 = t1;           //saves the current time
-    int errL = des_spdL - curr_spdL;
-    pwmL = pwmL + (errL * prop); // proportional
+    double errL = des_spdL - curr_spdL;
+    pwmL = pwmL + (errL * 2); // proportional
 
-    int errR = des_spdR - curr_spdR;
-    pwmR = pwmR + (errR * prop); // proportional
+    double errR = des_spdR - curr_spdR;
+    pwmR = pwmR + (errR * 2); // proportional
 
-    int errL2 = des_spdL2 - curr_spdL2;
-    pwmL2 = pwmL2 + (errL2 * prop); // proportional
-
-    int errR2 = des_spdR2 - curr_spdR2;
-    pwmR2 = pwmR2 + (errR2 * prop); // proportional
-
-    pwmL = (pwmL > MAX_PWM) ? MAX_PWM : pwmL;
-    pwmR = (pwmR > MAX_PWM) ? MAX_PWM : pwmR;
-    pwmL2 = (pwmL2 > MAX_PWM) ? MAX_PWM : pwmL2;
-    pwmR2 = (pwmR2 > MAX_PWM) ? MAX_PWM : pwmR2;
-
+    pwmL = (pwmL > 255) ? 255 : pwmL;
+    pwmR = (pwmR > 255) ? 255 : pwmR;
     pwmL = (pwmL < 0) ? 0 : pwmL;
     pwmR = (pwmR < 0) ? 0 : pwmR;
-    pwmL2 = (pwmL2 < 0) ? 0 : pwmL2;
-    pwmR2 = (pwmR2 < 0) ? 0 : pwmR2;
 
     analogWrite(pwm_pinL, pwmL);      //sets the desired speed
     analogWrite(pwm_pinR, pwmR);      //sets the desired speed
-    analogWrite(pwm_pinL2, pwmL2);      //sets the desired speed
-    analogWrite(pwm_pinR2, pwmR2);      //sets the desired speed
-
-    if(des_spdL == 0) 
-      pwmL = 0;
-    if(des_spdL2 == 0) 
-      pwmL2 = 0;
-    if(des_spdR == 0) 
-      pwmR = 0;
-    if(des_spdR2 == 0) 
-      pwmR2 = 0;
-
     publishSpeed(LOOPTIME);
   }
 
-  attachInterrupt(4, isrR2, RISING);
-  attachInterrupt(2, isrR, RISING);
-  attachInterrupt(5, isrL2, RISING);
-  attachInterrupt(3, isrL, RISING);
+  attachInterrupt(0, isrL, RISING);
+  attachInterrupt(1, isrR, RISING);
 
   nh.spinOnce();
-
 
 }
 
 //Publish function for odometry, uses a vector type message to send the data (message type is not meant for that but that's easier than creating a specific message type)
 void publishSpeed(double time) {
   speed_msg.header.stamp = nh.now();      //timestamp for odometry data
-  speed_msg.vector.x = state;    //left wheel speed (in m/s)
-  speed_msg.vector.y = pwmL2;   //right wheel speed (in m/s)
-  speed_msg.vector.z = des_spdL; //time / 1000;       //looptime, should be the same as specified in LOOPTIME (in s)
+  speed_msg.vector.x = curr_spdL;    //left wheel speed (in m/s)
+  speed_msg.vector.y = curr_spdL;   //right wheel speed (in m/s)
+  speed_msg.vector.z = time / 10;       //looptime, should be the same as specified in LOOPTIME (in s)
   speed_pub.publish(&speed_msg);
   //  nh.loginfo("Publishing odometry");
 }
